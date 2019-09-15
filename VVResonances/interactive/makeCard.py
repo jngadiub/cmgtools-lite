731bda4b0292de9e95c97df78ffd0da56f48b1af
IreneZoi <irene.zoi@cern.ch>
2019-09-15 18:53:21 +0200

    adding VV+VH datacards

--- VVResonances/interactive/makeCard.py
+++ VVResonances/interactive/makeCard.py
@@ -1,4 +1,4 @@
-from tools.DatacardTools import *
+1;95;0cfrom tools.DatacardTools import *
 import sys,os
 import ROOT
 ROOT.gSystem.Load("libHiggsAnalysisCombinedLimit")
@@ -19,17 +19,30 @@ lumi_unc = {'2016':1.025,'2017':1.023}
 scales = {"2017" :[0.983,1.08], "2016":[1.014,1.086]}
 scalesHiggs = {"2017" :[1.,1.], "2016":[1.,1.]}
 
+#quick fix to add VH !!!
+vtag_unc = {'VV_HPHP':{},'VV_HPLP':{},'VV_LPLP':{},'VH_HPHP':{},'VH_HPLP':{},'VH_LPHP':{}}
+vtag_unc['VV_HPHP'] = {'2016':'1.232/0.792','2017':'1.269/0.763'}
+vtag_unc['VV_HPLP'] = {'2016':'0.882/1.12','2017':'0.866/1.136'}    
+vtag_unc['VV_LPLP'] = {'2016':'1.063','2017':'1.043'}
+vtag_unc['VH_HPHP'] = {'2016':'1.232/0.792','2017':'1.269/0.763'}
+vtag_unc['VH_HPLP'] = {'2016':'0.882/1.12','2017':'0.866/1.136'}    
+vtag_unc['VH_LPHP'] = {'2016':'1.063','2017':'1.043'}
+
+vtag_pt_dependence = {'VV_HPHP':'((1+0.06*log(MH/2/300))*(1+0.06*log(MH/2/300)))','VV_HPLP':'((1+0.06*log(MH/2/300))*(1+0.07*log(MH/2/300)))','VH_HPHP':'((1+0.06*log(MH/2/300))*(1+0.06*log(MH/2/300)))','VH_HPLP':'((1+0.06*log(MH/2/300))*(1+0.07*log(MH/2/300)))','VH_LPHP':'((1+0.06*log(MH/2/300))*(1+0.06*log(MH/2/300)))'}
+
+'''
 vtag_unc = {'VV_HPHP':{},'VV_HPLP':{},'VV_LPLP':{}}
 vtag_unc['VV_HPHP'] = {'2016':'1.232/0.792','2017':'1.269/0.763'}
 vtag_unc['VV_HPLP'] = {'2016':'0.882/1.12','2017':'0.866/1.136'}    
 vtag_unc['VV_LPLP'] = {'2016':'1.063','2017':'1.043'}
 
 vtag_pt_dependence = {'VV_HPHP':'((1+0.06*log(MH/2/300))*(1+0.06*log(MH/2/300)))','VV_HPLP':'((1+0.06*log(MH/2/300))*(1+0.07*log(MH/2/300)))'}
-  
+'''  
 #purities= ['VV_HPLP']
-purities= ['VV_HPLP','VV_HPHP']
+#purities= ['VV_HPLP','VV_HPHP']
+purities= ['VV_HPLP','VV_HPHP','VH_HPLP','VH_HPHP','VH_LPHP']
 #signals = ["BulkGWW", "BulkGZZ","ZprimeWW","WprimeWZ","VprimeWV","'ZprimeZH'"]
-signals = ["BulkGVV"]
+signals = ["BulkGWW"]
 
 Tools = DatacardTools(scales,scalesHiggs,vtag_pt_dependence,lumi_unc,vtag_unc,sf_qcd,pseudodata,outlabel)
 
@@ -96,6 +109,8 @@ for sig in signals:
       print t2wcmd
       os.system(t2wcmd)
     del card
+
+    '''
     #make combined HPHP+HPLP card   
     combo_card = 'datacard_'+cat.replace("_HPHP","").replace("_HPLP","").replace("_LPLP","")+'.txt'
     combo_workspace = 'workspace_'+cat.replace("_HPHP","").replace("_HPLP","").replace("_LPLP","")+'.root'
@@ -106,6 +121,89 @@ for sig in signals:
     t2wcmd = "text2workspace.py %s -o %s"%(combo_card,combo_workspace)
     print t2wcmd
     os.system(t2wcmd)
+    '''
+
+    '''
+    #make combined VV HPHP+HPLP card
+    print "######### Make VV card ###########"
+    combo_card = 'datacard_'+cat.replace("VV_HPHP","VV").replace("VV_HPLP","VV").replace("VV_LPLP","")+'.txt'
+    print combo_card
+    combo_workspace = 'workspace_'+cat.replace("VV_HPHP","VV").replace("VV_HPLP","VV").replace("VV_LPLP","")+'.root'
+    print combo_workspace
+    os.system('rm %s'%combo_card)
+    cmd_combo+=' >> %s'%combo_card
+    print cmd_combo
+    os.system(cmd_combo)
+    t2wcmd = "text2workspace.py %s -o %s"%(combo_card,combo_workspace)
+    print t2wcmd
+    os.system(t2wcmd)
+    #make combined VH HPHP+HPLP+LPHP card
+    print "######### Make VH card ###########"
+    combo_card = 'datacard_'+cat.replace("VH_HPHP","VH").replace("VH_HPLP","VH").replace("VH_LPHP","")+'.txt'
+    print combo_card
+    combo_workspace = 'workspace_'+cat.replace("VH_HPHP","VH").replace("VH_HPLP","VH").replace("VH_LPHP","")+'.root'
+    print combo_workspace
+    os.system('rm %s'%combo_card)
+    cmd_combo+=' >> %s'%combo_card
+    print cmd_combo
+    os.system(cmd_combo)
+    t2wcmd = "text2workspace.py %s -o %s"%(combo_card,combo_workspace)
+    print t2wcmd
+    os.system(t2wcmd)
+    '''
+
+    '''
+    print "######### Make VV card ###########"   
+    combo_card_vv="datacard_JJ_BulkGWW_VV_13TeV_2016.txt"
+    combo_workspace_vv = "workspace_JJ_BulkGWW_VV_13TeV_2016.root"
+    os.system('rm %s'%combo_card_vv)
+    cmd_combo_vv+=' >> %s'%combo_card_vv
+    print cmd_combo_vv
+    os.system(cmd_combo_vv)
+    t2wcmd = "text2workspace.py %s -o %s"%(combo_card_vv,combo_workspace_vv)
+    print t2wcmd
+    os.system(t2wcmd)
+
+    print "######### Make VH card ###########"   
+    combo_card_vh="datacard_JJ_BulkGWW_VH_13TeV_2016.txt"
+    combo_workspace_vh = "workspace_JJ_BulkGWW_VH_13TeV_2016.root"
+    os.system('rm %s'%combo_card_vh)
+    cmd_combo_vh+=' >> %s'%combo_card_vh
+    print cmd_combo_vh
+    os.system(cmd_combo_vh)
+    t2wcmd = "text2workspace.py %s -o %s"%(combo_card_vh,combo_workspace_vh)
+    print t2wcmd
+    os.system(t2wcmd)
+
+    print "######### Make VV + VH card ###########"     
+    combo_card_tot="datacard_JJ_BulkGWW_TOT_13TeV_2016.txt"
+    combo_workspace_tot = "workspace_JJ_BulkGWW_TOT_13TeV_2016.root"
+    os.system('rm %s'%combo_card_tot)
+#    cmd_combo_tot+=' >> %s'%combo_card_tot
+    cmd_combo_tot="combineCards.py JJ_VV_13TeV_2016=datacard_JJ_BulkGWW_VV_13TeV_2016.txt JJ_VH_13TeV_2016=datacard_JJ_BulkGWW_VH_13TeV_2016.txt >> datacard_JJ_BulkGWW_TOT_13TeV_2016.txt"
+    print cmd_combo_tot
+    os.system(cmd_combo_tot)
+    t2wcmd = "text2workspace.py %s -o %s"%(combo_card_tot,combo_workspace_tot)
+    print t2wcmd
+    os.system(t2wcmd)
+
+    '''
+    #make combined VV+VH card
+    print "######### Make VV + VH card ###########"
+
+    combo_card = 'datacard_'+cat.replace("VV_HPHP","").replace("VV_HPLP","").replace("VV_LPLP","").replace("VH_HPHP","").replace("VH_HPLP","").replace("VH_LPHP","")+'.txt'
+
+    combo_workspace = 'workspace_'+cat.replace("VV_HPHP","").replace("VV_HPLP","").replace("VV_LPLP","").replace("VH_HPHP","").replace("VH_HPLP","").replace("VH_LPHP","")+'.root'
+
+    os.system('rm %s'%combo_card)
+    cmd_combo+=' >> %s'%combo_card
+    print cmd_combo
+    os.system(cmd_combo)
+    t2wcmd = "text2workspace.py %s -o %s"%(combo_card,combo_workspace)
+    print t2wcmd
+    os.system(t2wcmd)
+
+
   
   #make combine 2016+2017 card
   combo_card = 'datacard_'+cat.replace("_HPHP","").replace("_HPLP","").replace("_LPLP","").replace('_2016','').replace('_2017','')+'.txt'
