5e9746b9b3ed094395dfc2dddfdede2e960f1d64
IreneZoi <irene.zoi@cern.ch>
2020-07-17 15:43:58 +0200

    work in progress toward run2

--- VVResonances/interactive/makeCard.py
+++ VVResonances/interactive/makeCard.py
@@ -14,6 +14,7 @@ parser.add_option("--pseudodata",dest="pseudodata",help="make cards with real da
 parser.add_option("--signal",dest="signal",default="BulkGWW,BulkGZZ,ZprimeWW,ZprimeZH,WprimeWH,WprimeWZ",help="which signal do you want to run? options are BulkGWW, BulkGZZ, WprimeWZ, ZprimeWW, ZprimeZH")
 parser.add_option("--outlabel",dest="outlabel",help="lebel for output workspaces for example sigonly_M4500",default='')
 parser.add_option("-c","--category",dest="category",default="VV_HPLP,VV_HPHP,VH_HPLP,VH_HPHP,VH_LPHP",help="run period")
+parser.add_option("--combo",dest="combo",default=True,help="If True inputs from the 3 years combined will be used")
 
 
 (options,args) = parser.parse_args()
@@ -27,53 +28,80 @@ pseudodata = options.pseudodata
 
 outlabel = options.outlabel
 
-datasets= options.period.split(",")
-
 
 purities= options.category.split(",")
 
 signals = options.signal.split(",")
-
+print "signals ",signals
 doVjets= True
 sf_qcd=1.
 if outlabel.find("sigonly")!=-1 or outlabel.find("qcdonly")!=-1: doVjets = False
 if outlabel.find("sigonly")!=-1 or outlabel.find("Vjetsonly")!=-1: sf_qcd = 0.00001
 
-resultsDir = {'2016':'results_2016','2017':'results_2017'}
-
-
 
 # vtag uncertainty is added through the migrationunc.json file 
 # all other uncertainties and SF from one place: defined in init_VV_VH.json imported via the class defined in cuts.py
-ctx16 = cuts.cuts("init_VV_VH.json",2016,"dijetbins_random")
-ctx17 = cuts.cuts("init_VV_VH.json",2017,"dijetbins_random")
-ctx18 = cuts.cuts("init_VV_VH.json",2018,"dijetbins_random")
+ctx = cuts.cuts("init_VV_VH.json",options.period,"dijetbins_random")
+#ctx17 = cuts.cuts("init_VV_VH.json","2017","dijetbins_random")
+#ctx18 = cuts.cuts("init_VV_VH.json","2018","dijetbins_random")
 
-lumi = {'2016':ctx16.lumi,'2017':ctx17.lumi, '2018':ctx18.lumi}
-lumi_unc = {'2016':ctx16.lumi_unc,'2017':ctx17.lumi_unc, '2018':ctx18.lumi_unc}
+lumi = ctx.lumi #{'2016':ctx16.lumi["2016"],'2017':ctx17.lumi["2017"], '2018':ctx18.lumi}
+lumi_unc = ctx.lumi_unc #{'2016':ctx16.lumi_unc,'2017':ctx17.lumi_unc, '2018':ctx18.lumi_unc}
 
+lumitot=0
+for year in lumi.keys():
+  print year
+  lumitot+=lumi[year]
+print "lumi tot ",lumitot
+print "lumi ",lumi
+print type(lumi)
+
+
+
+# what are there scales?
 #scales = {"2017" :[ctx17.W_HPmassscale,ctx17.W_LPmassscale], "2016":[ctx16.W_HPmassscale,ctx16.W_LPmassscale], "2018":[ctx18.W_HPmassscale,ctx18.W_LPmassscale]}
 #scalesHiggs = {"2017" :[ctx17.H_HPmassscale,ctx17.H_LPmassscale], "2016":[ctx16.H_HPmassscale,ctx16.H_LPmassscale], "2018":[ctx18.H_HPmassscale,ctx18.H_LPmassscale]}
 
-scales = {"2017" :[1,1], "2016":[1,1], "2018":[1,1]}
+scales = {"2017" :[1,1], "2016":[1,1], "2018":[1,1], "Run2":[1,1]}
 scalesHiggs = {"2017" :[1,1], "2016":[1,1], "2018":[1,1]}
 
 #quick fix to add VH !!!
-vtag_pt_dependence = {"2016" : ctx16.vtag_pt_dependence,"2017" : ctx17.vtag_pt_dependence,"2018" : ctx18.vtag_pt_dependence}
+vtag_pt_dependence = ctx.vtag_pt_dependence #{"2016" : ctx.vtag_pt_dependence["2016"],"2017" : ctx.vtag_pt_dependence["2017"],"2018" : ctx.vtag_pt_dependence["2018"]}
 
 
-doCorrelation = False
-if sig.find("H") != -1 : doCorrelation = True
+
+datasets= options.period.split(",")
+resultsDir = {year:'results_'+year for year in datasets}
+
+if len(datasets) == 3 and options.combo == True:
+  datasets = []
+  datasets.append("Run2")
+  lumi.update({"Run2":lumitot})
+  resultsDir.update({"Run2" : "results_Run2"})
+  lumi_unc.update( {"Run2": 1.8 }) # from https://twiki.cern.ch/twiki/bin/view/CMS/TWikiLUM but maybe it should be moved to the init json
+print "datasets ",datasets
+print "result dir ",resultsDir
+
+
+
+
+
+
+doCorrelation = True 
 Tools = DatacardTools(scales,scalesHiggs,vtag_pt_dependence,lumi_unc,sf_qcd,pseudodata,outlabel,doCorrelation)
 
 
 for sig in signals:
+
   cmd ="combineCards.py"
   for dataset in datasets:
+    print dataset
     cmd_combo="combineCards.py"
     for p in purities:
 
       ncontrib = 0
+      print dataset," has lumi ",lumi[dataset]
+      print type(lumi[dataset])
       
       cat='_'.join(['JJ',sig,p,'13TeV_'+dataset])
       card=DataCardMaker('',p,'13TeV_'+dataset,lumi[dataset],'JJ',cat)
@@ -89,9 +117,10 @@ for sig in signals:
         print "##########################       including W/Z jets in datacard      ######################"
         rootFileMVV = resultsDir[dataset]+'/JJ_%s_WJets_MVV_'%dataset+p+'.root'    
         rootFileNorm = resultsDir[dataset]+'/JJ_%s_WJets_%s.root'%(dataset,p)
+        print rootFileMVV," ",rootFileNorm
         Tools.AddWResBackground(card,dataset,p,rootFileMVV,rootFileNorm,resultsDir[dataset],ncontrib)
         ncontrib+=1
-        
+        print " W jets done, now do Z"         
         rootFileMVV = resultsDir[dataset]+'/JJ_%s_ZJets_MVV_'%dataset+p+'.root'
         rootFileNorm = resultsDir[dataset]+"/JJ_%s_ZJets_%s.root"%(dataset,p)
         Tools.AddZResBackground(card,dataset,p,rootFileMVV,rootFileNorm,resultsDir[dataset],ncontrib)
@@ -99,7 +128,8 @@ for sig in signals:
 
       #rootFile3DPDF = resultsDir[dataset]+'/JJ_2016_nonRes_3D_VV_HPLP.root'
       print "##########################       including QCD in datacard      ######################"
-      rootFile3DPDF = resultsDir[dataset]+"/save_new_shapes_%s_pythia_"%dataset+p+"_3D.root"
+      rootFile3DPDF = resultsDir[dataset]+'/JJ_%s_nonRes_3D_'%dataset+'NP.root'            
+      #rootFile3DPDF = resultsDir[dataset]+"/save_new_shapes_%s_pythia_"%dataset+p+"_3D.root"
       #rootFile3DPDF = resultsDir[dataset]+"/save_new_shapes_%s_pythia_"%dataset+"VVVH_all"+"_3D.root"
       print "rootFile3DPDF ",rootFile3DPDF
       rootFileNorm = resultsDir[dataset]+"/JJ_%s_nonRes_"%dataset+p+".root"   
@@ -110,7 +140,7 @@ for sig in signals:
 
 
 #      rootFileData = resultsDir[dataset]+"/JJ_%s_nonRes_3D_%s.root"%(dataset,p) #use this only to prepare workspace for making pseudo data with vjets
-      rootFileData = resultsDir[dataset]+"/JJ_%s_nonRes_3D_none.root"%(dataset) #use this only to prepare workspace for making pseudo data with vjets
+      rootFileData = resultsDir[dataset]+"/JJ_%s_nonRes_3D_NP.root"%(dataset) #use this only to prepare workspace for making pseudo data with vjets
       histName="histo"
       scaleData=lumi[dataset]
 
@@ -126,7 +156,7 @@ for sig in signals:
         scaleData=1.0
       if pseudodata=="Vjets":
         print "Using pseudodata with vjets"
-        rootFileData = resultsDir[dataset]+"/JJ_PDVjets_"+p+".root"
+        rootFileData = resultsDir[dataset]+"/JJ_"+dataset+"_PDVjets_"+p+".root"
         histName="data"
         scaleData=1.0
       if pseudodata==sig:
@@ -140,7 +170,7 @@ for sig in signals:
       Tools.AddSigSystematics(card,sig,dataset,p,1)
       Tools.AddResBackgroundSystematics(card,p)
       Tools.AddNonResBackgroundSystematics(card,p)
-      Tools.AddTaggingSystematics(card,sig,dataset,p,resultsDir[dataset]+'/migrationunc.json')
+      #Tools.AddTaggingSystematics(card,sig,dataset,p,resultsDir[dataset]+'/migrationunc.json')
       print "##########################       systematics added in datacard      ######################"  
 
 
