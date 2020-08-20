dd899ca88de6cb79ee800d1fceda974a6d45f7a7
IreneZoi <irene.zoi@cern.ch>
2020-08-20 11:13:01 +0200

    added clarifying comments

--- VVResonances/interactive/PlotPulls.C
+++ VVResonances/interactive/PlotPulls.C
@@ -2,9 +2,9 @@ void PlotPulls()
 {
 
   int nfiles = 1;
-  const int nlabels = 25;  
+  const int nlabels = 23; //25;  // number of nuissance parameters
   std::string labels[nlabels] = {""};
-
+  // To get the fitDiagnostics.root file run ~ this command  'combine -M FitDiagnostics -m 2000 results_2016/workspace_JJ_BulkGWW_VH_NPHP_control_region_13TeV_2016_newdata.root' on the workspace 
   TFile* tf = new TFile("fitDiagnostics.root","READ");
   tf->ls();
   
