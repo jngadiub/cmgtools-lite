2233ee1885585e22bd08b15bf981e2822402ee56
IreneZoi <irene.zoi@cern.ch>
2020-08-25 15:05:23 +0200

    added check for VBF signals

--- VVResonances/interactive/runFitPlots_vjets_signal_bigcombo_splitRes.py
+++ VVResonances/interactive/runFitPlots_vjets_signal_bigcombo_splitRes.py
@@ -57,7 +57,8 @@ if options.name.find("BulkGWW")!=-1:
     signalName="BulkGWW"
 if options.name.find("BulkGZZ")!=-1:
     signalName="BulkGZZ"
-
+if options.name.find("VBF")!=-1:
+    signalName="VBF_"+signalName
 
 def addGraph(graphs):
     if graphs[0]==None: return None
