cb935727424c3f3b964f1d168f42ba2e127c58af
IreneZoi <irene.zoi@cern.ch>
2020-10-01 16:12:51 +0200

    do also prefit

--- VVResonances/interactive/makeTTbarFits.sh
+++ VVResonances/interactive/makeTTbarFits.sh
@@ -6,23 +6,29 @@
 #make the ws for ttbar only -> for the purpose of doing a ttbar only fit to get the true yields for the different ttbar contributions for the final fit 
 # the workspaces need to be made with ttbar pseudodata from the ttbar-MC sample
 
-category=("VH_NPHP_control_region") # "VH_HPLP" "VH_LPHP" "VH_HPHP" "VV_HPLP" "VV_HPHP")
+#category=("VH_NPHP_control_region") # "VH_HPLP" "VH_LPHP" "VH_HPHP" "VV_HPLP" "VV_HPHP")
+category=("VH_LPHP" "VH_HPLP" "VH_HPHP" "VV_HPLP" "VV_HPHP")
+
 for c in ${category[@]}
 do 
-    echo "############### make card for category ${c} ##########"
-    echo ${c}
-    python makeCard.py --signal BulkGWW --outlabel ttbar -c ${c} -p 2016 --pseudodata ttbar
-#done
 
-    outputdir=postfitTTonly2016/
+    outputdir=prefit_ttbar_2016/
     mkdir ${outputdir}
-#for c in ${category[@]}
-#do
-    echo "############## make postfit for ##############"
-    label=pseudoTTbar2016_postfit_${c}
+
+    echo "############## make prefit for ##############"
+    label=prefit_ttbar_2016_${c}
     echo $label
-    python runFitPlots_vjets_signal_bigcombo_splitRes.py -n workspace_JJ_BulkGWW_${c}_13TeV_2016ttbar2.root  -i  results_2016//JJ_2016_nonRes_${c}.root -M 2000  -o ${outputdir} --channel ${c} -l ${label} --doVjets --addTop --doFit | tee ${label}.log
-  
+    python runFitPlots_vjets_signal_bigcombo_splitRes.py -n workspace_JJ_BulkGWW_${c}_13TeV_2016_ttbar.root  -i  results_2016/JJ_2016_nonRes_${c}.root -M 2000  -o ${outputdir} --channel ${c} -l ${c} --doVjets --addTop | tee ${label}.log
+
+
+    outputdir=postfit_ttbar_2016/
+    mkdir ${outputdir}
+
+    echo "############## make postfit for ##############"
+    label=postfit_ttbar_2016_${c}
+    echo $label
+    python runFitPlots_vjets_signal_bigcombo_splitRes.py -n results_2016/workspace_JJ_BulkGWW_${c}_13TeV_2016_ttbar.root  -i  results_2016/JJ_2016_nonRes_${c}.root -M 2000  -o ${outputdir} --channel ${c} -l ${c} --doVjets --addTop --doFit | tee ${label}.log
+
 done
   
 ## the fit produces output files named workspacename.json -> these are then included in the makeCard script to extract the yields if NOT pseudodata=ttbar
