3671c7c2319021555d329af2413d27eefb74f63d
IreneZoi <irene.zoi@cern.ch>
2020-10-15 14:20:09 +0200

    small bug fix

--- VVResonances/interactive/tools/PostFitTools.py
+++ VVResonances/interactive/tools/PostFitTools.py
@@ -526,7 +526,7 @@ class Postfitplotter():
         if hsig!= None: # and (self.options.name.find('sigonly')!=-1  and doFit==0):
             print "print do hsignal ", hsig.Integral()
             if hsig.Integral()!=0.:   
-                hsig.Scale(scaling/normsig)
+                hsig.Scale(scaling/normsig.getVal())
         #        print "sig integral ",hsig.Integral()
         #        hsig.Scale(scaling/hsig.Integral())
         
@@ -1043,8 +1043,8 @@ class Projection():
         
         if pdf_sig!=None:
             print "fill signal "
-            for ik,iv in self.Bins_redux.iteritems(): self.htot_sig.Fill(iv,self.lv1_sig[0][iv]*norm_sig[0]); # print self.lv1_sig[0][iv]*norm_sig[0]    
-     
+            for ik,iv in self.Bins_redux.iteritems(): self.htot_sig.Fill(iv,self.lv1_sig[0][iv]*norm_sig[0].getVal()); # print self.lv1_sig[0][iv]*norm_sig[0]
+
         self.htot.Add(self.htot_nonres)
         if self.htot_Wres!=None: self.htot.Add(self.htot_Wres)
         if self.htot_Zres!=None: self.htot.Add(self.htot_Zres)
@@ -1080,8 +1080,8 @@ class Projection():
         x_min = self.Binslowedge
         proj = self.axis
         rand = ROOT.TRandom3(1234);
-        number_errorband = 5 #100
-        print " number_errorband reduced to "+str(number_errorband)+" for speed reasons!"
+        number_errorband = 100
+        #print " number_errorband reduced to "+str(number_errorband)+" for speed reasons!"
         syst = [0 for i in range(number_errorband)]
       
         value = [0 for x in range(len(x_min))]  
